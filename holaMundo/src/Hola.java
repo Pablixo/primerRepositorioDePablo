45de3277581b2b0e03cd31310580b32d3571b9b3
root <root@pablo-VirtualBox>
2013-02-26 09:14:48 +0000

    Pablo

--- holaMundo/src/Hola.java
+++ holaMundo/src/Hola.java
@@ -7,5 +7,10 @@ public class Hola
 		System.out.println("Mierda");
 		System.out.println("AHHHHHHHHH");
 		System.out.println("HOLA");
+		System.out.println("CAMBIANDO CON ADELAY");
+	}
+	public void remoto()
+	{
+		System.out.println("Cambiando remotamente");
 	}
 }
